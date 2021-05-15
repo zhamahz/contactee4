eaa821209d4c22b946373f69c670d32e1376528c
AisuluuDzh <dzhumabekova.aisuluu@gmail.com>
2021-05-15 15:16:30 +0600

    commit

---
+++ views/university/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\University */
+
+$this->title = 'Create University';
+$this->params['breadcrumbs'][] = ['label' => 'Universities', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="university-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ views/university/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\University */
+
+$this->title = 'Update University: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Universities', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="university-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
